59b6bd5b247557c7c0c11b35e9c6e35cac5cffe3
yongrean <78528865+k08200@users.noreply.github.com>
2026-04-03 01:04:24 +0900

    fix: Uint8Array buffer type cast for push subscription

--- packages/web/src/components/push-register.tsx
+++ packages/web/src/components/push-register.tsx
@@ -40,7 +40,7 @@ async function registerPush() {
 
       subscription = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicKey),
+        applicationServerKey: urlBase64ToUint8Array(publicKey).buffer as ArrayBuffer,
       });
     }
 
